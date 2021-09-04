53736826539bbcdb0f8670bad9192f929d0a823b
dessintadr <dessintarahmadn@gmail.com>
2021-09-04 15:12:24 +0700

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import CardList from "./components/CardList";
+import Quotes from "./components/Quotes";
+import Gambar from "./assets/images/gambar1.png";
+const quotes =
+  "Disaat payung teduh ku dengar , disitu lah tugastugas terbabat abis";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    <CardList
+      name="shaddam"
+      nim="211201181..."
+      kelompok="0"
+      isNameBold
+      image={Gambar}
+    />
+    <Quotes author="Orang" quotes={quotes} />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
